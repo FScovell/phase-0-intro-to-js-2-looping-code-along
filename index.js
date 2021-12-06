34e7e4a83b5410af57ffca94de238bc0ac2f1731
Frederick <admin@freddyscovell.com>
2021-12-06 16:44:38 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const cards = [];
+
+function writeCards(names) {
+    let greetings = [];
+    for (let i = 0; i < names.length; i++) {
+        greetings[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        
+    }
+    return greetings;
+}
+
+writeCards(cards);
+
+function countDown(duke) {
+    let i = duke;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
